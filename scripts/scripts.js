cb144cdead6919c751bdb85f063e02ce94f75977
Andrew Hartley <acalebh@gmail.com>
2018-08-29 13:16:24 -0400

    added new cat picture and updated click listener

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,16 @@
 //define cat to be clicked
-const cat = document.getElementById('cat');
+const cats = document.getElementsByClassName('cat');
+//define cat names in array
+const catNames = ['Meowster', 'Claws']
 
-//numClicks is the counter that will increment when cat is clicked
+//numClicks is the counter that will increment when a cat is clicked
 let numClicks = 0;
 
-//this is the event listener for a click on the #cat element
-cat.addEventListener('click', function(event) {
-	numClicks++;
-	document.getElementById('counter').innerText = 'Click Count: ' + numClicks;
-	console.log(numClicks);
-});
+//this is the event listener for a click on any .cat element
+for (c=0; c<cats.length; c++) {
+	cats[c].addEventListener('click', function(event) {
+		numClicks++;
+		document.getElementById('counter').innerText = 'Click Count: ' + numClicks;
+		
+	});
+}
