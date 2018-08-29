5b75075c5ab4d3560f29b13f5f93f5151afe6f10
Andrew Hartley <acalebh@gmail.com>
2018-08-28 23:34:35 -0400

    the original cat clicker everyone!

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,12 @@
-//TODO: add click counter to page
+//define cat to be clicked
+const cat = document.getElementById('cat');
+
+//numClicks is the counter that will increment when cat is clicked
+let numClicks = 0;
+
+//this is the event listener for a click on the #cat element
+cat.addEventListener('click', function(event) {
+	numClicks++;
+	document.getElementById('counter').innerText = 'Click Count: ' + numClicks;
+	console.log(numClicks);
+});
